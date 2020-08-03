de9e1010611d77d4c291921d226316f91ab6657d
sfujiwara <shuhei.fujiwara@gmail.com>
2020-08-03 16:27:58 +0900

    rm all files

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-gcloud ml-engine local train \
-  --module-name trainer.task \
-  --package-path trainer \
-  --distributed \
-  --parameter-server-count 1 \
-  --worker-count 1
